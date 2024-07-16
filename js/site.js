15372df9d053e349a42c7d0bc43ae0f45e09e20e
lmatulis <matulisluke@gmail.com>
2024-07-16 14:02:22 -0500

    init Commit

--- js/site.js
+++ js/site.js
@@ -1,12 +1,29 @@
-//
-function getValues(message) {
-    alert(message);
-}
+
 
 //Get values from the screen and display
-function getValues2() {
-    let message = document.getElementById("message").value
-    let alertbox = document.getElementById("alertBox")
+function getValues() {
+    let inputMessage = document.getElementById("message").value;
+    let alertMessage = document.getElementById("alertBox");
+    let reversedMessage = reverse(inputMessage);
 
-    alertbox.innerText = message
+    alertMessage.innerHTML = displayMessage(inputMessage, reversedMessage);
+}
+
+function reverse(inputMessage) {
+let reversedMessage = "";
+    for(let i = inputMessage.length-1; i >= 0; i--) {
+        reversedMessage += inputMessage[i];
+    }
+    return reversedMessage;
+}
+
+function displayMessage(inputMessage, reversedMessage) {
+
+    let alertElement = document.getElementById("alertBox");
+
+    //alertElement.classList.remove("d-none");
+    alertElement.style.opacity = 1;
+
+    let message = `<div>The message, <strong>${inputMessage}</strong> in reverse is: <strong>${reversedMessage}</strong></div>`;
+    return message;
 }
